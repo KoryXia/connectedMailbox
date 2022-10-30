53c212e7c38a873a6e02b46497a92a5122e6fc74
XIA <koryxia@icloud.com>
2022-10-30 13:59:34 +0100

    Initial commit

---
+++ src/main/java/com/noralsy/connectedmailbox/ConnectedMailboxApplication.java
@@ -0,0 +1,13 @@
+package com.noralsy.connectedmailbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedMailboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectedMailboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noralsy/connectedmailbox/ConnectedMailboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noralsy.connectedmailbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectedMailboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
