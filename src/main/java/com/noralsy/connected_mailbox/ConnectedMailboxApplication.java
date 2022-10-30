ed16bd6dac2279c710d8ee7dc41baaf1dfb9b668
XIA <koryxia@icloud.com>
2022-10-30 17:55:25 +0100

    basic apis

--- src/main/java/com/noralsy/connected_mailbox/ConnectedMailboxApplication.java
+++ src/main/java/com/noralsy/connected_mailbox/ConnectedMailboxApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.noralsy.connected_mailbox.Mapper")
+@MapperScan("com.noralsy.connected_mailbox.mapper")
 public class ConnectedMailboxApplication {
 
     public static void main(String[] args) {
